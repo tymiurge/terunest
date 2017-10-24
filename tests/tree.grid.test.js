a8910a53da7fc71b6a1986612560c1884dad27e9
tymiurge <tmrlabas@gmail.com>
2017-10-24 16:40:00 +0300

    TEST: preparing test environments

---
+++ tests/tree.grid.test.js
@@ -0,0 +1,11 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+
+import TreeGrid from './../src/components/tree-grid'
+
+describe('TreeGrid.functional', () => {
+
+    it('dummy test', () => {
+        expect(true).toBe(false)
+    })
+})
