75347af18daa6d10dab2e5af254bbc4b9141f99c
tymiurge <tmrlabas@gmail.com>
2017-10-24 17:03:46 +0300

    FIX: removing runs history

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
 import { Container, Menu } from 'semantic-ui-react'
 import RunsList from './components/runs-list'
-import RunsHistory from './components/runs-history'
 import RunDetails from './components/run-details'
 
 class App extends Component {
-  state = { activeItem: 'runs history' }
+  state = { activeItem: 'runs list' }
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
   render() {
@@ -15,17 +14,12 @@ class App extends Component {
     return (
       <Container fluid={true}>
         <Menu pointing secondary>
-          <Menu.Item name='runs history' active={activeItem === 'runs history'} onClick={this.handleItemClick} />
           <Menu.Item name='runs list' active={activeItem === 'runs list'} onClick={this.handleItemClick} />
           {
           showRunDetails &&
           <Menu.Item name='run details' active={activeItem === 'run details'} onClick={this.handleItemClick} />
           }
         </Menu>
-        {
-          activeItem === 'runs history' &&
-          <RunsHistory />
-        }
         {
           activeItem === 'runs list' &&
           <RunsList />
