cac4967f876ff54ce52f9d8d00bbc9242c163943
tymiurge <tmrlabas@gmail.com>
2017-11-07 14:28:36 +0200

    FEATURE: expanding/collapsing panels in test-details.js

--- src/components/test-details.js
+++ src/components/test-details.js
@@ -4,23 +4,37 @@ import TestSteps from './test-steps'
 import ConsoleErrors from './console-errors'
 
 class TestDetails extends Component {
+
+    constructor (props) {
+        super(props)
+        this.state = {
+            steps: true,
+            consoleErrors: true
+        }
+    }
+
+    toggle = panelName => {
+        const changes = { [panelName]: !this.state[panelName] }
+        this.setState(Object.assign({}, this.state, changes))
+    }
+
     render () {
         return (
             <Accordion styled fluid>
-                <Accordion.Title active={true} index={0}>
+                <Accordion.Title active={this.state.steps} onClick={() => this.toggle('steps')}>
                     <Icon name='dropdown' />
                     Steps
-                    <Label color='blue' style={{marginLeft: '5px'}}horizontal>{this.props.steps.length}</Label>
+                    <Label color='blue' style={{marginLeft: '5px'}} horizontal>{this.props.steps.length}</Label>
                 </Accordion.Title>
-                <Accordion.Content active={true}>
+                <Accordion.Content active={this.state.steps}>
                     <TestSteps steps={this.props.steps} />
                 </Accordion.Content>
-                <Accordion.Title active={true} index={1}>
+                <Accordion.Title active={this.state.consoleErrors} onClick={() => this.toggle('consoleErrors')}>
                     <Icon name='dropdown' />
                     Console Errors
-                    <Label color='blue' style={{marginLeft: '5px'}}horizontal>{this.props.consoleErrors.length}</Label>
+                    <Label color='red' style={{marginLeft: '5px'}} horizontal>{this.props.consoleErrors.length}</Label>
                 </Accordion.Title>
-                <Accordion.Content active={true}>
+                <Accordion.Content active={this.state.consoleErrors}>
                     <ConsoleErrors consoleErrors={this.props.consoleErrors} />
                 </Accordion.Content>
             </Accordion>    
