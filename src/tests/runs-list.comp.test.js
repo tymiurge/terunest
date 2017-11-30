cd9df53bcc24df7f51e04f93b929666a794900c5
tymiurge <tmrlabas@gmail.com>
2017-11-30 21:19:10 +0200

    TEST: component runs-list tests, now working

--- src/tests/runs-list.comp.test.js
+++ src/tests/runs-list.comp.test.js
@@ -8,20 +8,46 @@ import RunsList from './../components/runs-list'
 
 configure({ adapter: new Adapter() })
 
+const runsListPageObj = {
+
+    setFilterValue: (wrapper, value) => {
+        const filterInput = wrapper.find('input').first()
+        filterInput.simulate('change', {target: {value: 'build 00013'}})
+    },
+
+    getDisplayedRunsCount: wrapper => wrapper.find('table tbody TableRow').length
+}
+
 describe('runs-list component tests', () => {
     
-    it('runs filtering: positive', () => {
+    it('runs filtering: text in filter is not encounter in table', () => {
+        store.runs.setList(runsFixture)
+        const component = mount( <RunsList store={store} /> )
+        runsListPageObj.setFilterValue(component, 'build 00013')
+        const remainingRuns = runsListPageObj.getDisplayedRunsCount(component)
+        expect(remainingRuns).toEqual(0)
+    })
+
+    it('runs filtering: text in filter is encounter in table 1 time', () => {
         store.runs.setList(runsFixture)
         const component = mount(
             <RunsList store={store} />
         )
-        const filterInput = component.find('input')
-        filterInput.simulate('focus')
-        filterInput.simulate('change', {target: {value: 'new_value'}})
-        filterInput.simulate('keyDown', { key: 'Enter' })
-        //filterInput.simulate('keydown', { which: 't' })
-        console.log('input = ' + filterInput.get(0).value)
-        // expect(runTitleIsInEachItem).toBe(true)
+        const filterInput = component.find('input').first()
+        filterInput.simulate('change', {target: {value: 'build 0001'}})
+        const remainingRuns = component.find('table tbody TableRow').length
+        expect(remainingRuns).toEqual(1)
+    })
+
+    it('runs filtering: text in filter is encountered in all test runs', () => {
+        store.runs.setList(runsFixture)
+        const component = mount(
+            <RunsList store={store} />
+        )
+        const filterInput = component.find('input').first()
+        filterInput.simulate('change', {target: {value: 'build 000'}})
+        const remainingRuns = component.find('table tbody TableRow').length
+        expect(remainingRuns).toEqual(2)
     })
 
 })
